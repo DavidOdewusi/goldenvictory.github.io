08fc12b028de227b77e27ff653cc22bd80d12524
David Odewusi <davidodewusi@gmail.com>
2021-06-17 14:48:50 +0100

    Added js animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -9,4 +9,18 @@ $(document).ready(function() {
     }, {
         offset: '60px'
     });
+
+    $('.js--register-btn').click(function() {
+        $('html, body').animate({scrollTop: $('.js--footer').offset().top}, 1000);
+    });
+
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '100px'
+    });
+
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animate__animated animate__fadeIn');
+    });
 });
