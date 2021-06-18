1534bb17ad9dd7b3800a1c8223d0d503a4fa7887
David Odewusi <davidodewusi@gmail.com>
2021-06-18 01:07:34 +0100

    Added changes

--- resources/js/script.js
+++ resources/js/script.js
@@ -21,6 +21,38 @@ $(document).ready(function() {
     });
 
     $('.js--wp-2').waypoint(function(direction){
-        $('.js--wp-2').addClass('animate__animated animate__fadeIn');
+        $('.js--wp-2').addClass('animate__animated animate__fadeInLeft');
+    },{
+        offset: '300px'
+    });
+
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animate__animated animate__backInLeft');
+    },{
+        offset: '600px'
+    });
+
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animate__animated animate__backInUp');
+    },{
+        offset: '600px'
+    });
+
+    $('.js--wp-5').waypoint(function(direction){
+        $('.js--wp-5').addClass('animate__animated animate__backInRight');
+    },{
+        offset: '600px'
+    });
+
+    $('.js--wp-6').waypoint(function(direction){
+        $('.js--wp-6').addClass('animate__animated animate__fadeIn');
+    },{
+        offset: '400px'
+    });
+
+    $('.js--wp-7').waypoint(function(direction){
+        $('.js--wp-7').addClass('animate__animated animate__fadeIn');
+    },{
+        offset: '400px'
     });
 });
